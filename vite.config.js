8705c84f4cb69a0575ab24d0ef8f82588f639a5c
dotsatya <satyasundardey5@gmail.com>
2025-07-03 01:51:35 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Formal-Portfolio/', // 👈 IMPORTANT: GitHub repo name with slashes
   plugins: [react()],
 })
