b6c4880b414c3ec883d0acaaa5d40a560efb7914
dotsatya <satyasundardey5@gmail.com>
2025-08-07 01:40:43 +0530

    for github deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/Formal-Portfolio/', // 👈 IMPORTANT: GitHub repo name with slashes
-  base: '/',
+  // base: '/',
   plugins: [react()],
 })
