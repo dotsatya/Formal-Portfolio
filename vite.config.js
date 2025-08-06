73f3a4753da8a35095e2e2205b1b016ef7c06ae1
dotsatya <satyasundardey5@gmail.com>
2025-08-06 23:55:52 +0530

    fixed deployment issues 1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Formal-Portfolio/', // 👈 IMPORTANT: GitHub repo name with slashes
+  // base: '/Formal-Portfolio/', // 👈 IMPORTANT: GitHub repo name with slashes
+  base: '/',
   plugins: [react()],
 })
